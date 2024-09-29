cdc8a89642ee70612c3a670602a471a8ae6ab799
0kate <o.keito317@gmail.com>
2024-09-29 03:42:37 +0000

    update x86_64 config

---
+++ machines/x86_64.nix
@@ -0,0 +1,32 @@
+{ ... }:
+{
+  imports = [
+    ./shared.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # VMware, Parallels both only support this being 0 otherwise you see
+  # "error switching console mode" on boot.
+  boot.loader.systemd-boot.consoleMode = "0";
+
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+    desktopManager.plasma5 = true;
+    displayManager.lightdm = true;
+  };
+
+  fonts = {
+    fontDir.enable = true;
+
+    packages = [
+      pkgs.fira-code
+      pkgs.jetbrains-mono
+    ];
+  };
+
+  system.stateVersion = "24.05";
+}
