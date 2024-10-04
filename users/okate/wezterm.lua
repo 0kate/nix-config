62f9b083d39898d37680481d9097e1fdd3f54227
0kate <o.keito317@gmail.com>
2024-10-04 13:27:23 +0900

    update wezterm config

--- users/okate/wezterm.lua
+++ users/okate/wezterm.lua
@@ -1,7 +1,12 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'Ayu Mirage (Gogh)'
+-- Fix window title with "WezTerm"
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  return 'WezTerm'
+end)
+
+config.color_scheme = 'AtomOneLight'
 config.colors = {
   cursor_bg = '#666666',
 }
@@ -14,9 +19,13 @@ config.enable_scroll_bar = false
 config.font = wezterm.font_with_fallback({
   -- { family = 'Hack Nerd Font' },
   -- { family = 'Hack Nerd Font', assume_emoji_presentation = true },
-  { family = 'JetBrains Mono' },
-  { family = 'JetBrains Mono', assume_emoji_presentation = true },
+  -- { family = 'Monaspace Neon Var', weight = 'Medium' },
+  -- { family = 'Monaspace Neon Var', weight = 'Medium', assume_emoji_presentation = true, harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, },
+  { family = 'JetBrains Mono', weight = 'Light', harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } },
+  { family = 'JetBrains Mono', weight = 'Light', assume_emoji_presentation = true, harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } },
 })
+config.harfbuzz_features = { 'zero' }
+config.hide_tab_bar_if_only_one_tab = true
 config.line_height = 1.2
 config.use_ime = true
 config.window_decorations = 'RESIZE'
