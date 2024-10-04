d0691b4ef851604189571fb09514d7f35a51ba45
keito-osaki <o.keito317@gmail.com>
2024-10-05 00:31:52 +0900

    add mkSystem utility

---
+++ lib/mkSystem.nix
@@ -0,0 +1,27 @@
+{ nixpkgs, overlays, inputs }:
+name:
+{ system, user, wsl ? false }:
+let
+  machineConfig   = ../machines/${name};
+  userHomeConfig = ../users/${user}/home.nix;
+  userOSConfig   = ../users/${user}/nixos.nix;
+
+  home-manager = inputs.home-manager;
+in nixpkgs.lib.nixosSystem {
+  inherit system;
+
+  modules = [
+    { nixpkgs.overlays = overlays; }
+    { nixpkgs.config.allowUnfree = true; }
+    machineConfig
+    userOSConfig
+    home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.${user} = import userHomeConfig {
+        inputs = inputs;
+        isWSL  = false;
+      };
+    }
+  ];
+}
