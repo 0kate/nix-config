40b1f2e80bc7835a3e57b8eb026cd401502e8a35
keito-osaki <o.keito317@gmail.com>
2024-10-06 07:01:11 +0900

    add okate user to networkmanager group

--- users/okate/nixos.nix
+++ users/okate/nixos.nix
@@ -6,7 +6,7 @@
     isNormalUser = true;
     home = "/home/okate";
     hashedPassword = "$y$j9T$ubiv8LXGhk3bttXtvfCQ9/$QU9AuQnzCsTgkb43UOHULj0Al3sC5VRGgkZFs6K5w83";
-    extraGroups = [ "docker" "wheel" ];
+    extraGroups = [ "docker" "networkmanager" "wheel" ];
     shell = pkgs.zsh;
   };
 }
