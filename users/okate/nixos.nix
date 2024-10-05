ccc4e56a251b9e83cd72ed61f2ffc209f999d4c8
0kate <o.keito317@gmail.com>
2024-10-05 19:31:51 +0900

    add hashed password for user

--- users/okate/nixos.nix
+++ users/okate/nixos.nix
@@ -5,6 +5,7 @@
   users.users."okate" = {
     isNormalUser = true;
     home = "/home/okate";
+    hashedPassword = "$y$j9T$ubiv8LXGhk3bttXtvfCQ9/$QU9AuQnzCsTgkb43UOHULj0Al3sC5VRGgkZFs6K5w83";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.zsh;
   };
