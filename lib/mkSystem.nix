25584360a108fbb3ef1cf76ca00c613e68225ae9
keito-osaki <o.keito317@gmail.com>
2024-10-14 07:49:49 +0900

    add nix-ld configs

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -8,6 +8,8 @@ let
   userHomeConfig = ../users/${user}/home.nix;
   userOSConfig   = ../users/${user}/nixos.nix;
 
+  nix-ld = inputs.nix-ld;
+
   home-manager = inputs.home-manager;
 
   hardware = inputs.nixos-hardware;
@@ -15,6 +17,7 @@ in nixpkgs.lib.nixosSystem {
   inherit system;
 
   modules = [
+    nix-ld.nixosModules.nix-ld
     hardware.nixosModules.lenovo-thinkpad-t14
     { nixpkgs.overlays = overlays; }
     { nixpkgs.config.allowUnfree = true; }
