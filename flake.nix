d27196a966bfab0df1a17ca491a9e7eb790495c4
keito-osaki <o.keito317@gmail.com>
2025-01-05 23:05:03 +0900

    update nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     nix-ld.url = "github:Mic92/nix-ld";
@@ -16,7 +16,7 @@
     devenv.url = "github:cachix/devenv";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
