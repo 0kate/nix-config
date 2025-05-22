ca2cf3e32db4e5418a2ec0cc8acbf5eb2867ba09
keito-osaki <o.keito317@gmail.com>
2025-05-22 21:03:13 +0900

    update flake config

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
@@ -15,16 +16,17 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    ghostty.url = "github:ghostty-org/ghostty";
-    thorium.url = "github:0kate/nix-thorium";
   };
 
   outputs = { nixpkgs, home-manager, nixos-wsl, ... }@inputs:
   let
     system = "x86_64-linux";
 
-    overlays = [];
+    overlays = [
+      (final: prev: rec {
+        helix = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.helix;
+      })
+    ];
 
     mkSystem = import ./lib/mkSystem.nix {
       inherit nixpkgs overlays inputs;
