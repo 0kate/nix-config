31fd766475a548ecaadcf38ddcb4cd7b04590e7d
keito-osaki <o.keito317@gmail.com>
2025-05-22 22:56:25 +0900

    split alacritty settings to another file

---
+++ users/okate/alacritty/settings.nix
@@ -0,0 +1,24 @@
+{
+  general = {
+    import = [ "~/.config/alacritty/themes/sonokai.toml" ];
+  };
+
+  window = {
+    decorations = "full";
+  };
+
+  font = {
+    normal = {
+      family = "Hack Nerd Font";
+      style = "Light";
+    };
+    bold = {
+      family = "Hack Nerd Font";
+      style = "Bold";
+    };
+    italic = {
+      family = "Hack Nerd Font";
+      style = "Italic";
+    };
+  };
+}
