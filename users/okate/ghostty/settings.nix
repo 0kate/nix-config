277be69ecbd66cc644d9daebc303e6463a9231d4
0kate <o.keito317@gmail.com>
2026-01-08 08:33:47 +0900

    add ghostty settings

---
+++ users/okate/ghostty/settings.nix
@@ -0,0 +1,13 @@
+{
+  font-family = "Hack Nerd Font"; 
+  font-feature = "-calt";
+
+  theme = "GitHub Dark Dimmed";
+  
+  keybind = [
+    "ctrl+shift+h=goto_split:left"
+    "ctrl+shift+j=goto_split:down"
+    "ctrl+shift+k=goto_split:up"
+    "ctrl+shift+l=goto_split:right"
+  ];
+}
