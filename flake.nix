cc91ae364cc6d658a8a2505ed7d5002eabcbb5ff
0kate <o.keito317@gmail.com>
2026-05-15 20:08:09 +0900

    update nix-config

--- flake.nix
+++ flake.nix
@@ -1,56 +1,27 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-25.11";
-    };
-
-    nixpkgs-unstable = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    nix-flatpak = {
-      url = "github:gmodena/nix-flatpak/?ref=latest";
-    };
 
-    nixgl = {
-      url = "github:nix-community/nixGL";
-    };
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
   };
 
   outputs =
-    {
-      nixpkgs,
-      nixpkgs-unstable,
-      home-manager,
-      nix-flatpak,
-      nixgl,
-      ...
-    }:
+    { nixpkgs, home-manager, nix-flatpak, ... }:
     let
       system = "x86_64-linux";
-
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ nixgl.overlay ];
-        config.allowUnfree = true;
-      };
-
-      pkgsUnstable = import nixpkgs-unstable {
-        inherit system;
-        overlays = [ nixgl.overlay ];
         config.allowUnfree = true;
       };
     in
     {
       homeConfigurations."okate" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        extraSpecialArgs = { inherit pkgsUnstable; };
-
         modules = [
           nix-flatpak.homeManagerModules.nix-flatpak
           ./users/okate/home.nix
--- users/okate/home.nix
+++ users/okate/home.nix
@@ -1,9 +1,13 @@
-{ pkgs, pkgsUnstable, ... }:
+{ pkgs, config, lib, ... }:
 let
   # starship
   starshipSettings = import ./starship/settings.nix;
 
+  # oh-my-posh
+  ohMyPoshSettings = import ./oh-my-posh/settings.nix;
+
   # ghostty
+  ghosttyVersion = "1.3.1";
   ghosttySettings = import ./ghostty/settings.nix;
   ghosttyFormat = pkgs.formats.keyValue {
     listsAsDuplicateKeys = true;
@@ -49,8 +53,8 @@ in
       hackgen-nf-font
       udev-gothic-nf
 
-      pkgsUnstable.claude-code
-      pkgsUnstable.devbox
+      claude-code
+      devbox
       difftastic
       direnv
       eza
@@ -68,6 +72,29 @@ in
       ripgrep
       yazi
     ];
+    activation = {
+      integrateGhosttyDirect = config.lib.dag.entryAfter ["writeBoundary"] ''
+        echo "Install Ghostty via AppImage and Gear Lever."
+
+        # Add required commands (curl, flatpak) to PATH
+        export PATH=$PATH:${pkgs.curl}/bin:${pkgs.flatpak}/bin
+
+        # Define the temporary file path and URL
+        APP_IMAGE_FILE="Ghostty-${ghosttyVersion}-x86_64.AppImage"
+        URL="https://github.com/pkgforge-dev/ghostty-appimage/releases/download/v${ghosttyVersion}/$APP_IMAGE_FILE"
+        TMP_FILE="/tmp/$APP_IMAGE_FILE"
+
+        # 1. Download the file to a temporary directory
+        curl -L -sS "$URL" -o "$TMP_FILE"
+        chmod +x "$TMP_FILE"
+
+        # 2. Integrate via Gear Lever CLI
+        flatpak run it.mijorus.gearlever --integrate --yes --replace "$TMP_FILE"
+
+        # 3. Clean up the temporary file (in case it was not moved)
+        rm -f "$TMP_FILE"
+      '';
+    };
   };
   
   services = {
@@ -96,6 +123,7 @@ in
 
     zsh = {
       enable = true;
+      dotDir = "${config.xdg.configHome}/zsh";
       initContent = builtins.readFile ./zsh/extrarc;
       enableCompletion = true;
       autosuggestion.enable = true;
@@ -128,6 +156,12 @@ in
       enable = true;
       enableZshIntegration = true;
     };
+
+    # oh-my-posh = {
+    #   enable = true;
+    #   enableZshIntegration = true;
+    #   settings = ohMyPoshSettings;
+    # };
 
     starship = {
       enable = true;
@@ -184,7 +218,6 @@ in
     helix = {
       enable = true;
       defaultEditor = true;
-      package = pkgsUnstable.helix;
       settings = helixSettings;
       languages = helixLanguages;
     };
@@ -200,23 +233,65 @@ in
   dconf = {
     enable = true;
     settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        clock-format = "24h";
+        gtk-key-theme = "Emacs";
+      };
+      "org/gnome/desktop/input-sources" = {
+        sources = [ (lib.hm.gvariant.mkTuple [ "xkb" "us" ]) ];
+        xkb-options = [ "ctrl:swapcaps" ];
+      };
       "org/gnome/shell" = {
-        # enabled-extensions = [
-        #   "Bluetooth-Battery-Meter@maniacx.github.com"
-        #   "clipboard-indicator@tudmotu.com"
-        #   "dash-to-dock@micxgx.gmail.com"
-        #   "kimpanel@kde.org"
-        #   "paperwm@paperwm.github.com"
-        #   "pomodoro@arun.codito.in"
-        #   "system-monitor@gnome-shell-extensions.gcampax.github.com"
-        # ];
-        # "extensions/paperwm/keybindings" = {
-        #   switch-next = "[ '<Super>period' '<Alt>d' '<Super>d' ]";
-        #   switch-previous = "[ '<Super>comma' '<Alt>s' '<Super>s' ]";
-        # };
+        enabled-extensions = [
+          "Bluetooth-Battery-Meter@maniacx.github.com"
+          "clipboard-indicator@tudmotu.com"
+          "dash-to-dock@micxgx.gmail.com"
+          "kimpanel@kde.org"
+          "paperwm@paperwm.github.com"
+          "system-monitor@gnome-shell-extensions.gcampax.github.com"
+        ];
+        disabled-extensions = [
+          "background-logo@fedorahosted.org"
+          "bluetooth-battery-monitor@v8v88v8v88.com"
+          "dash-to-panel@jderose9.github.com"
+          "just-perfection-desktop@just-perfection"
+          "sysmonitor@talhasiddique7"
+          "window-list@gnome-shell-extensions.gcampax.github.com"
+        ];
+        favorite-apps = [
+          "org.gnome.Calendar.desktop"
+          "org.gnome.Nautilus.desktop"
+          "org.gnome.Settings.desktop"
+          "org.gnome.Software.desktop"
+          "com.google.Chrome.desktop"
+          "com.slack.Slack.desktop"
+          "ghostty.desktop"
+          "md.obsidian.Obsidian.desktop"
+        ];
+      };
+      "org/gnome/shell/extensions/paperwm" = {
+        horizontal-margin = 20;
+        selection-border-radius-bottom = 10;
+        selection-border-radius-top = 10;
+        selection-border-size = 8;
+        show-window-position-bar = false;
+        show-workspace-indicator = false;
+        vertical-margin = 16;
+        vertical-margin-bottom = 16;
+        window-gap = 18;
+      };
+      "org/gnome/shell/extensions/paperwm/keybindings" = {
+        switch-left = [ "<Alt>s" ];
+        switch-right = [ "<Alt>d" ];
+      };
+      "org/gnome/mutter" = {
+        attach-modal-dialogs = false;
+        edge-tiling = false;
+        workspaces-only-on-primary = false;
       };
       "org/gnome/TextEditor" = {
-        "keybindings" = "vim";
+        keybindings = "vim";
       };
     };
   };
